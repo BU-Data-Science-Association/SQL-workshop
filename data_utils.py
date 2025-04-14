9def402b14683a46fc23cb4c479cd9eb2b6c1997
Sam Cowan <75958455+samc5@users.noreply.github.com>
2025-04-14 02:51:59 +0000

    updated data_utils for DHall (all dining halls)

--- data_utils.py
+++ data_utils.py
@@ -9,7 +9,7 @@ marciano= pd.read_csv("/workspace/Marciano.csv")
 marciano_calories = pd.read_csv("/workspace/Marciano_calories.csv")
 granby = pd.read_csv("/workspace/Granby.csv")
 granby_calories = pd.read_csv("/workspace/Granby_calories.csv")
-
+dhall = pd.read_csv("/workspace/dining.csv")
 
 engine = create_engine("postgresql://app_user:app_password@localhost:5432/app")  # adjust accordingly
 warren.to_sql("Warren", engine, if_exists="replace", index=False)
@@ -19,6 +19,6 @@ west_calories.to_sql("West_Calories", engine, if_exists="replace", index=False)
 marciano.to_sql("Marciano", engine, if_exists="replace", index=False)
 marciano_calories.to_sql("Marciano_Calories", engine, if_exists="replace", index=False)
 granby.to_sql("Granby", engine, if_exists="replace", index=False)
-granby_calories.to_sql("Granby_Common", engine, if_exists="replace", index=False)
-
+granby_calories.to_sql("Granby_Calories", engine, if_exists="replace", index=False)
+dhall.to_sql("Dhall", engine, if_exists="replace", index=False)
 print("Data should be loaded into Postgres")
